6aa4b0a261beca9eee3dfecbe15a6a7ebc2480d3
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-12 16:22:15 +0400

    payment

--- apps/intro/src/index.ts
+++ apps/intro/src/index.ts
@@ -1,9 +1,17 @@
+import { xrpToDrops } from "xrpl";
+import { sendPaymentSimple } from "./transactions";
+import { WALLET_1, WALLET_2 } from "./wallet";
 import { getXrplClient } from "./xrpl-client";
 
 const client = getXrplClient();
 
 const main = async () => {
   await client.connect();
+
+  await sendPaymentSimple(
+    { Destination: WALLET_2.address, Amount: xrpToDrops(20) },
+    { wallet: WALLET_1 }
+  );
 
   await client.disconnect();
 };
