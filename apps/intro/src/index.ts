3efb36513a17657211d7fe1dc6130027c4451dc0
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-12 14:56:16 +0400

    clients and wallet

--- apps/intro/src/index.ts
+++ apps/intro/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello")
+import { getXrplClient } from "./xrpl-client";
+
+const client = getXrplClient();
+
+const main = async () => {
+  await client.connect();
+
+  await client.disconnect();
+};
+
+main();
--- apps/intro/src/wallet.ts
+++ apps/intro/src/wallet.ts
@@ -4,6 +4,8 @@ import { Wallet } from "xrpl";
 dotenv.config();
 
 // https://xrpl.org/xrp-testnet-faucet.html
-const WALLET_1_SEED = process.env.WALLET_1_SEED; // Edit/create a .env file with a WALLET_1_SEED key
-const WALLET_2_SEED = process.env.WALLET_2_SEED; // Edit/create a .env file with a WALLET_2_SEED key
-const WALLET_3_SEED = process.env.WALLET_3_SEED; // Edit/create a .env file with a WALLET_3_SEED key
+const WALLET_1_SEED = process.env.WALLET_1_SEED ?? ""; // Edit/create a .env file with a WALLET_1_SEED key
+const WALLET_2_SEED = process.env.WALLET_2_SEED ?? ""; // Edit/create a .env file with a WALLET_2_SEED key
+
+export const WALLET_1 = Wallet.fromSeed(WALLET_1_SEED);
+export const WALLET_2 = Wallet.fromSeed(WALLET_2_SEED);
