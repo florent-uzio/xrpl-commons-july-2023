1177e057d8763e2b52a6a23eb2d485caee554f7a
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-13 12:51:16 +0400

    nfts

--- apps/intro/src/index.ts
+++ apps/intro/src/index.ts
@@ -1,6 +1,5 @@
-import { xrpToDrops } from "xrpl";
-import { sendPaymentSimple } from "./transactions";
-import { WALLET_1, WALLET_2 } from "./wallet";
+import { acceptNftOffer } from "./transactions";
+import { WALLET_2 } from "./wallet";
 import { getXrplClient } from "./xrpl-client";
 
 const client = getXrplClient();
@@ -8,9 +7,33 @@ const client = getXrplClient();
 const main = async () => {
   await client.connect();
 
-  await sendPaymentSimple(
-    { Destination: WALLET_2.address, Amount: xrpToDrops(20) },
-    { wallet: WALLET_1 }
+  await client.fundWallet(WALLET_2);
+
+  //   await sendPaymentSimple(
+  //     { Destination: WALLET_2.address, Amount: xrpToDrops(20) },
+  //     { wallet: WALLET_1 }
+  //   );
+
+  //   await mintNft(
+  //     {
+  //       URI: "https://media.giphy.com/media/EYJidsvKRt9a8/giphy.gif",
+  //       NFTokenTaxon: 0,
+  //     },
+  //     { wallet: WALLET_1 }
+  //   );
+
+  //   await createNftOffer(
+  //     {
+  //       Flags: NFTokenCreateOfferFlags.tfSellNFToken,
+  //       NFTokenID: "00080000336B93F98530EE29346D545777E44CC34FE045DA0000099B00000000",
+  //       Amount: "100",
+  //     },
+  //     { wallet: WALLET_1 }
+  //   );
+
+  await acceptNftOffer(
+    { NFTokenSellOffer: "16BD0F642DD356DFE735371A24AC3CAB1CBBFB3D43BCAA170A8C7015A00D901F" },
+    { wallet: WALLET_2 }
   );
 
   await client.disconnect();
---
+++ apps/intro/src/transactions/nfts.ts
@@ -0,0 +1,96 @@
+import {
+  NFTokenAcceptOffer,
+  NFTokenCreateOffer,
+  NFTokenCreateOfferFlags,
+  NFTokenMint,
+  NFTokenMintFlags,
+  convertStringToHex,
+  xrpToDrops,
+} from "xrpl";
+import { TxnOptions } from "../models";
+import { getXrplClient } from "../xrpl-client";
+
+type MintNftProps = Omit<NFTokenMint, "TransactionType" | "Account">;
+
+const client = getXrplClient();
+
+export const mintNft = async ({ URI, Flags, ...rest }: MintNftProps, { wallet }: TxnOptions) => {
+  console.log("******* LET'S MINT AN NFT *******");
+  console.log();
+
+  // Construct the base transaction
+  const transaction: NFTokenMint = {
+    TransactionType: "NFTokenMint",
+    Account: wallet.address,
+    Flags: Flags ?? NFTokenMintFlags.tfTransferable,
+    URI: URI ? convertStringToHex(URI) : "",
+    ...rest,
+  };
+
+  // Autofill transaction with additional fields, sign and submit
+  const result = await client.submitAndWait(transaction, { autofill: true, wallet });
+
+  console.log("Result Mint NFT", result);
+  return result;
+};
+
+/**
+ * ************************************
+ */
+
+type CreateNftOfferProps = Omit<NFTokenCreateOffer, "TransactionType" | "Account"> &
+  (
+    | { Flags: NFTokenCreateOfferFlags.tfSellNFToken; Owner?: never }
+    | { Flags?: undefined; Owner: string }
+  );
+
+export const createNftOffer = async (
+  { Amount, ...rest }: CreateNftOfferProps,
+  { wallet }: TxnOptions
+) => {
+  console.log("******* LET'S CREATE AN NFT OFFER *******");
+  console.log();
+
+  // Convert the amount to drops (1 drop = .000001 XRP)
+  if (typeof Amount === "string") {
+    Amount = xrpToDrops(Amount);
+  }
+
+  // Construct the base transaction
+  const transaction: NFTokenCreateOffer = {
+    Account: wallet.address,
+    Amount,
+    TransactionType: "NFTokenCreateOffer",
+    ...rest,
+  };
+
+  // Autofill transaction with additional fields, sign and submit
+  const result = await client.submitAndWait(transaction, { autofill: true, wallet });
+
+  console.log("Result Create NFT Offer", result);
+  return result;
+};
+
+/**
+ * ************************************
+ */
+
+type AcceptNftOfferProps = Omit<NFTokenAcceptOffer, "TransactionType" | "Account">;
+
+export const acceptNftOffer = async (props: AcceptNftOfferProps, { wallet }: TxnOptions) => {
+  console.log("******* LET'S ACCEPT AN NFT OFFER *******");
+  console.log();
+
+  // Construct the base transaction
+  const transaction: NFTokenAcceptOffer = {
+    Account: wallet.address,
+    TransactionType: "NFTokenAcceptOffer",
+    ...props,
+  };
+
+  // Autofill transaction with additional fields, sign and submit
+  const result = await client.submitAndWait(transaction, { autofill: true, wallet });
+
+  console.log("Result Accept NFT Offer", result);
+  return result;
+};
