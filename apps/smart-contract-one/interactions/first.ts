80686200ae23c4e30bad6d4a5f87aae47fc38990
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-20 15:56:43 +0400

    scripts and typing

--- apps/smart-contract-one/interactions/first.ts
+++ apps/smart-contract-one/interactions/first.ts
@@ -1,54 +1,25 @@
-import { Web3 } from "web3";
-import contract from "../artifacts/contracts/first.sol/FirstContract.json";
-import { factories } from "../typechain-types";
-const contractAddress = "0xB267cFfF4cF526D969617b8283529F7539c2C49A";
-const contractABI = contract.abi;
+import { ethers } from "hardhat";
+
+import { FirstContract__factory } from "../typechain-types";
+
+// Update after each deployment
+const contractAddress = "0xDF9dC0ad0409Fd8871FC0Cf71499217511A857B4";
 
 const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8]; // Example array of numbers
 
-const web3Provider = new Web3.providers.HttpProvider("https://rpc-evm-sidechain.xrpl.org");
-const web3 = new Web3(web3Provider);
-const contractInstance = new web3.eth.Contract(
-  factories.firstSol.FirstContract__factory.abi,
+const URL_RPC = "https://rpc-evm-poa-sidechain.peersyst.tech";
 
-  // contractABI,
-  contractAddress
-);
+async function interactWithContract() {
+  const provider = new ethers.JsonRpcProvider(URL_RPC);
+  const signer = await provider.getSigner();
 
-// contractInstance.methods
-//   .calculateSum(numbers)
-//   .call<unknown>()
-//   .then((result) => {
-//     const sum = typeof result === "bigint" ? result.toString() : result;
-//     console.log("Sum:", sum);
-//   })
-//   .catch((error: Error) => {
-//     console.error("Error:", error);
-//   });
+  // Access the contract factory using the generated typings
+  const contractFactory = FirstContract__factory.connect(contractAddress, signer);
 
-// contractInstance.methods
-//   .greetOwner("Florent Uzio")
-//   .call<unknown>()
-//   .then((result) => {
-//     // const sum = typeof result === "bigint" ? result.toString() : result;
-//     console.log(result);
-//   })
-//   .catch((error: Error) => {
-//     console.error("Error:", error);
-//   });
+  const message = await contractFactory.getMessage();
+  const calc = await contractFactory.calculateSum(numbers);
 
-// contractInstance.methods
-//   .setMessage("This is a test")
-//   .call()
-//   .then((result) => console.log(result))
-//   .catch((err) => {
-//     console.log(err);
-//   });
+  console.log({ message, calc: Number(calc) });
+}
 
-contractInstance.methods
-  .getMessage()
-  .call()
-  .then((result) => console.log(result))
-  .catch((err) => {
-    console.log(err);
-  });
+interactWithContract();
