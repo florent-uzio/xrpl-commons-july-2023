c4237ff9542933df94a735710c04ba704530e735
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-28 11:13:15 +0200

    chakra ui

--- apps/gemwallet/src/App.tsx
+++ apps/gemwallet/src/App.tsx
@@ -1,5 +1,13 @@
+import { Container, Heading } from "@chakra-ui/react";
+
 function App() {
-  return <div>Hello GemWallet !</div>;
+  return (
+    <Container>
+      <Heading py="10" as="h1" size="2xl" textAlign="center">
+        Hello GemWallet !
+      </Heading>
+    </Container>
+  );
 }
 
 export default App;
