4c7f99cb73fb3587f90b309d923b3bca3c0d9cde
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-06-30 12:41:02 +0200

    provider

--- apps/gemwallet/src/main.tsx
+++ apps/gemwallet/src/main.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./app";
+import { GemWalletProvider } from "./shared/contexts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <GemWalletProvider>
+        <App />
+      </GemWalletProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
