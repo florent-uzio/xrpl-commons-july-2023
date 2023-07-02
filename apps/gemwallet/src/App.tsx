d448262e5413774b7e8f0a957c7f891e3ddfd609
Florent <36513774+florent-uzio@users.noreply.github.com>
2023-07-02 16:21:32 +0400

    example update

--- apps/gemwallet/src/App.tsx
+++ apps/gemwallet/src/App.tsx
@@ -3,9 +3,9 @@ import { Example } from "./components";
 
 function App() {
   return (
-    <Container>
+    <Container minW="800">
       <Heading py="10" as="h1" size="2xl" textAlign="center">
-        Hello GemWallet !
+        Hello GemWallet!
       </Heading>
 
       {/* Insert your components here to interact with GemWallet */}
